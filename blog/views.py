1ddfc337228d364c4bf73f7c3bf9d172ecd8e79c
RodrigodeOliveiraPeixer <rodrigo.oliveira.peixer@gmail.com>
2022-12-01 18:25:14 -0300

    controleusuario

--- blog/views.py
+++ blog/views.py
@@ -2,29 +2,34 @@ from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    
 
 
 class BlogDetailView(DetailView):
     model = Post
     template_name= 'post_detail.html'
+    
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Post
     template_name = 'post_new.html'
     fields= "__all__"
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields=['title', 'body']
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Post
     template_name= 'post_delete.html'
     success_url = reverse_lazy('home')
