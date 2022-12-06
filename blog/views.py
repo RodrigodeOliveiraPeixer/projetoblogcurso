aab59e17d78cb3ff74f86fb291a4b6fa712057ab
RodrigodeOliveiraPeixer <109769171+RodrigodeOliveiraPeixer@users.noreply.github.com>
2022-12-05 23:22:24 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class BlogListView(ListView)
-    model = Po
+    model = Post
     template_name = 'home.html'
     
 
